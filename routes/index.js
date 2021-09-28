0225cdbc0adedcbaddcb11a7c254ce2e800290ac
Kunj-Choksi <kunj07soni@gmail.com>
2021-09-28 14:09:05 +0530

    Added Logo, Service page and Fixed style for landing page

--- routes/index.js
+++ routes/index.js
@@ -6,4 +6,8 @@ router.get('/', function(req, res, next) {
   res.render('index', { title: 'Express' });
 });
 
+router.get('/services', function(req, res, next) {
+  res.render('landing/services', { title: 'Express' });
+});
+
 module.exports = router;
