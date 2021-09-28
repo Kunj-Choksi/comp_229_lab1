7eb47e067c01d83a94f0d0f2d2d743821c6fd7c1
Kunj-Choksi <kunj07soni@gmail.com>
2021-09-29 00:01:15 +0530

    Added footer, favicon, projects page Fixed href links

--- routes/index.js
+++ routes/index.js
@@ -2,12 +2,16 @@ var express = require('express');
 var router = express.Router();
 
 /* GET home page. */
-router.get('/', function(req, res, next) {
-  res.render('index', { title: 'Express' });
+router.get('/', function (req, res, next) {
+    res.render('index', {});
 });
 
-router.get('/services', function(req, res, next) {
-  res.render('landing/services', { title: 'Express' });
+router.get('/services', function (req, res, next) {
+    res.render('landing/services', {});
+});
+
+router.get('/projects', function (req, res, next) {
+    res.render('landing/projects', {});
 });
 
 module.exports = router;
